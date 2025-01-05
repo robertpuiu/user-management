40f8611acd2102309c1ec88b4d118ac5b4bdf993
robertpuiu <puiurobert9441@yahoo.com>
2025-01-05 18:13:32 +0200

    ban users from posting

--- user_management/main/views.py
+++ user_management/main/views.py
@@ -2,22 +2,42 @@ from django.shortcuts import render, redirect
 from .forms import RegisterForm, PostForm
 from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User, Group
 from .models import Post
 # Create your views here.
-@login_required(login_url='/login')
+@login_required(login_url="/login")
 def home(request):
     posts = Post.objects.all()
 
-    if request.method == 'POST': # for some reason, DELETE requests are not working
-        post_id = request.POST.get('post-id')
-        post = Post.objects.filter(id=post_id).first()
-        if post and (post.author == request.user or request.user.has_perm('main.delete_post')):
-            post.delete()
+    if request.method == "POST":
+        post_id = request.POST.get("post-id")
+        user_id = request.POST.get("user-id")
 
-    return render(request, 'main/home.html', {'posts': posts})
+        if post_id:
+            post = Post.objects.filter(id=post_id).first()
+            if post and (post.author == request.user or request.user.has_perm("main.delete_post")):
+                post.delete()
+        elif user_id:
+            user = User.objects.filter(id=user_id).first()
+            if user and request.user.is_staff:
+                try:
+                    group = Group.objects.get(name='default')
+                    group.user_set.remove(user)
+                except:
+                    pass
+
+                try:
+                    group = Group.objects.get(name='mod')
+                    group.user_set.remove(user)
+                except:
+                    pass
+
+    return render(request, 'main/home.html', {"posts": posts})
 
 @login_required(login_url='/login')
 def create_post(request):
+    if not request.user.has_perm("main.add_post"):
+        return redirect('/home')
     if request.method == 'POST':
         form = PostForm(request.POST)
         if form.is_valid():
